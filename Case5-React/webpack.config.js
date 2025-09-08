25bfb63422368dfdd2322262cc7b1e9e8d7ef4f0
Süleyman Emir Kaya <129848831+emirfb7@users.noreply.github.com>
2025-09-08 22:27:21 +0300

    css problem solved

--- Case5-React/webpack.config.js
+++ Case5-React/webpack.config.js
@@ -1,60 +1,25 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: {
-    script: ['./src/script1.js', './src/script2.js'],
-    styles: './src/styles.js'
+    main: ['./src/script1.js', './src/script2.js', './src/styles.js']
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].min.js',
-    clean: true
+    filename: 'bundle.js'
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'style.min.css'
-    })
-  ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: true,
-            drop_debugger: true
-          }
-        }
-      }),
-      new CssMinimizerPlugin()
-    ]
-  },
-  mode: 'production',
+  mode: 'development',
   devServer: {
-    static: [
-      {
-        directory: path.join(__dirname, 'src'),
-        publicPath: '/src'
-      },
-      {
-        directory: path.join(__dirname, 'dist'),
-        publicPath: '/dist'
-      }
-    ],
-    compress: true,
+    static: path.join(__dirname, 'src'),
     port: 3001,
-    open: true,
-    hot: true
+    open: true
   }
 };

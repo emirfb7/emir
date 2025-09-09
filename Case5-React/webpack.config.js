9a40514e14787a245a13da39a1e5b0bc6bf48b79
Süleyman Emir Kaya <129848831+emirfb7@users.noreply.github.com>
2025-09-09 09:24:03 +0300

    dist

--- Case5-React/webpack.config.js
+++ Case5-React/webpack.config.js
@@ -1,25 +1,39 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: {
-    main: ['./src/script1.js', './src/script2.js', './src/styles.js']
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  mode: 'development',
-  devServer: {
-    static: path.join(__dirname, 'src'),
-    port: 3001,
-    open: true
-  }
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    entry: {
+      script: ['./src/script1.js', './src/script2.js', './src/styles.js']
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].min.js',
+      clean: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader'
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'style.min.css'
+      })
+    ],
+    mode: argv.mode || 'development',
+    devServer: {
+      static: path.join(__dirname, 'src'),
+      port: 3001,
+      open: true
+    }
+  };
 };
